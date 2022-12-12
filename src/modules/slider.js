984487724f2d030a7090b1745e89d8f81b92d926
aismeg <iaskja20@gmail.com>
2022-12-12 21:06:38 +0300

    slider in progress

---
+++ src/modules/slider.js
@@ -0,0 +1,51 @@
+/*BETA*/
+const slider = () => {
+    const topSlider = document.querySelector('.top-slider');
+    const item = topSlider.querySelectorAll('.item');
+    const timeInt = 3000;
+
+    let currentSlide = 0;
+    let timeId;
+
+    const prevSlide = (elems, index) => {
+        elems[index].style.display = "none";
+    };
+    const nextSlide = (elems, index) => {
+        elems[index].style.display = "block";
+    };
+
+    const autoSlide = () => {
+        prevSlide(item, currentSlide);
+
+        currentSlide++;
+
+        if (currentSlide >= item.length) {
+            currentSlide = 0;
+        }
+
+        nextSlide(item, currentSlide);
+    };
+
+    const startSlide = (timer = 1500) => {
+        timeId = setInterval(autoSlide, timer);
+    };
+    const stopSlide = () => {
+        clearInterval(timeId);
+    };
+
+
+    topSlider.addEventListener('mouseenter', (e) => {
+        if (e.target.matches('.top-slider')) {
+            stopSlide();
+        }
+    }, true);
+    topSlider.addEventListener('mouseleave', (e) => {
+        if (e.target.matches('.top-slider')) {
+            startSlide(timeInt);
+        }
+    }, true);
+
+    startSlide(timeInt);
+};
+
+export default slider;
