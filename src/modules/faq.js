40f1a198d8b9d75c04619b0a14752da8494f3084
aismeg <iaskja20@gmail.com>
2022-12-17 15:44:03 +0300

    ok

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -2,17 +2,16 @@ const faq = () => {
     const accordeon = document.querySelector('.accordeon');
     const element = accordeon.querySelectorAll('.element');
     const content = accordeon.querySelectorAll('.element-content');
+    let active = accordeon.querySelector('.active');
 
-    for (let i = 0; i < element.length; i++) {
-        element[i].addEventListener('click', () => {
-            const active = document.querySelector('.active');
+    element.forEach(e => {
+        e.addEventListener('click', () => {
+            active.classList.remove('active');
+            e.classList.add('active');
 
-            if (active) {
-                active.classList.remove('active');
-            }
+            active = accordeon.querySelector('.active');
         });
-    }
-
+    });
 };
 
 export default faq;
